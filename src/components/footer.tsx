07c4f876925b1d00410dbc7e6a8946e42ccb97be
Virginie <vbouvarel@lilo.org>
2021-07-17 13:16:13 +0200

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <>
       <footer>
-        <p>Made with love 💜</p>
+        <p>Made with 💜🎶😊😜🤷‍♂️🤦‍♂️🙌</p>
         <p>by Virginie Bouvarel ©2021</p>
       </footer>
     </>
