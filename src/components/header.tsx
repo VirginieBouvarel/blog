455d94cebd7713965f90e0e0aa828fcf826d1532
Virginie <vbouvarel@lilo.org>
2021-07-17 12:15:03 +0200

    update title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,6 @@ const navItems: { label: string; page?: string; link?: string }[] = [
   { label: 'Home', page: '/' },
   { label: 'Blog', page: '/blog' },
   { label: 'Contact', page: '/contact' },
-  { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
 ]
 
 const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
@@ -19,7 +18,7 @@ const Header = ({ titlePre = '' }) => {
   return (
     <header className={styles.header}>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
+        <title>{titlePre ? `${titlePre} |` : ''} Virginie's Blog</title>
         <meta
           name="description"
           content="An example Next.js site using Notion for the blog"
